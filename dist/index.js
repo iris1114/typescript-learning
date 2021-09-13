53d4321bd698a2d85fac626e639e74ecb6f1eea8
iris1114 <qweichew@gmail.com>
2021-09-13 23:05:58 +0800

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var index = "this's index";
+console.log(index);
+require("./hello");
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+
+const index:string = "this's index";
+console.log(index);
+
+import "./hello";
