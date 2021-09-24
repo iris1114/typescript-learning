5ad6dd1f21b64d8279ff45e1f094ec8ea98a5a1a
irischew1114 <qweichew@gmail.com>
2021-09-25 00:07:02 +0800

    update day9

--- day09/dist/index.js
+++ day09/dist/index.js
@@ -1,11 +1,13 @@
 "use strict";
 // Day09: 【TypeScript 學起來】物件型別 Object Types : arrays / function
 //arrays
-// 1.「型別 + 方括號」表示法
+//1.一般方括號寫法
 var list1 = [1, 2, 3];
-//2.陣列泛型
+//2.「型別 + 方括號」表示法
 var list2 = [1, 2, 3];
-var list3 = [1, 1, 2, 3, 5];
+//3.陣列泛型
+var list3 = [1, 2, 3];
+var list4 = [1, 1, 2, 3, 5];
 //function
 //Parameter Type Annotations 參數型別註釋
 function greet(name) {
@@ -15,6 +17,19 @@ function greet(name) {
 function getFavoriteNumber() {
     return 26;
 }
+//Function Declaration 函式宣告
+function sum(x, y) {
+    return x + y;
+}
+console.log(sum(1, 2)); //3
+//Function Expression 函式表示式
+var sum2 = function (x, y) {
+    return x + y;
+};
+console.log(sum2(1, 2)); //3
+// Arrow Function 箭頭函式
+var sum3 = function (x, y) { return x + y; };
+console.log(sum3(1, 2)); //3
 //Anonymous Functions 匿名函式
 var names = ["Alice", "Bob", "Eve"];
 names.forEach(function (s) {
--- day09/src/index.ts
+++ day09/src/index.ts
@@ -1,17 +1,20 @@
 // Day09: 【TypeScript 學起來】物件型別 Object Types : arrays / function
 
 //arrays
-// 1.「型別 + 方括號」表示法
-const list1: number[] = [1, 2, 3];
+//1.一般方括號寫法
+const list1 = [1, 2, 3];
 
-//2.陣列泛型
-const list2: Array<number> = [1, 2, 3]; 
+//2.「型別 + 方括號」表示法
+const list2: number[] = [1, 2, 3];
 
-//3.  用 interface (介面)表示陣列
+//3.陣列泛型
+const list3: Array<number> = [1, 2, 3]; 
+
+//4. 用 interface (介面)表示陣列
 interface NumberArray {
     [index: number]: number;
 }
-const list3: NumberArray = [1, 1, 2, 3, 5];
+const list4: NumberArray = [1, 1, 2, 3, 5];
 
 
 
@@ -21,13 +24,35 @@ function greet(name: string) {
   console.log("Hello, " + name.toUpperCase() + "!!");
 }
 
+
 //Return Type Annotations 返回值型別註釋
 function getFavoriteNumber(): number {
   return 26;
 }
+
+
+//Function Declaration 函式宣告
+function sum(x: number, y: number): number {
+  return x + y;
+}
+console.log(sum(1,2)); //3
+
+
+//Function Expression 函式表示式
+let sum2 = function (x: number, y: number): number {
+  return x + y;
+};
+console.log(sum2(1,2)); //3
+
+
+// Arrow Function 箭頭函式
+let sum3 = (x: number, y: number): number =>  x + y;
+console.log(sum3(1,2)); //3
+
 
 //Anonymous Functions 匿名函式
 const names = ["Alice", "Bob", "Eve"];
 names.forEach( (s) => {
     console.log(s.toUpperCase());
 });
+
